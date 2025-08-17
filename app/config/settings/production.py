006de86ba1925004d644b714f6364c8c0d9615f1
Bobur Yusupov <bobur.yu06@mail.ru>
2025-08-17 23:59:19 +0200

    Simplifies ALLOWED_HOSTS handling and adjusts volume spacing
    Replaces dynamic ALLOWED_HOSTS parsing with a static wildcard to reduce complexity and ensure consistent behavior.
    
    Adds a newline in the Docker Compose production file to improve formatting and adhere to file conventions.

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -4,9 +4,9 @@ import os
 DEBUG = False
 
 # Allow environment variable to specify multiple hosts
-ALLOWED_HOSTS = (
-    os.getenv("ALLOWED_HOSTS", "*").split(",") if os.getenv("ALLOWED_HOSTS") else []
-)
+ALLOWED_HOSTS = [
+    "*",
+]
 
 MIDDLEWARE += [  # noqa: F405
     "whitenoise.middleware.WhiteNoiseMiddleware",
